69c75b08c814c0864914530114ee0cfbd11832cf
Khanh Nguyen <155944105+hi-encacap@users.noreply.github.com>
2024-03-07 05:54:21 +0000

    feat: implement axios

--- src/libs/env.ts
+++ src/libs/env.ts
@@ -2,11 +2,14 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  client: {},
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string(),
+  },
   server: {
     NEXT_APP_LOGTAIL_SOURCE_TOKEN: z.string(),
   },
   runtimeEnv: {
     NEXT_APP_LOGTAIL_SOURCE_TOKEN: process.env.NEXT_APP_LOGTAIL_SOURCE_TOKEN,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
 });
